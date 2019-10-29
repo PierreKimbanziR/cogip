336fed70fa57591a3fe2d325475ba74a284363b8
Philippe Gilles <philgdep@gmail.com>
2019-10-29 10:17:14 +0100

    branch phil

--- index.php
+++ index.php
@@ -5,21 +5,37 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     exit;
 }
 
-require 'controllers/controller.php';
+require 'controllers/Controller.php';
 
 $url         = $_GET['p'];
 $explode_url = explode('/', $url);
 
-echo "<pre>";
-print_r($explode_url);
-echo "</pre>";
+// Debug router
+// echo "<pre>";
+// print_r($explode_url);
+// echo "</pre>";
 
-if ($explode_url[0] == 'contacts') {
+//----------
+//  HOME
+//----------
+
+if ($explode_url[0] == 'home') {
+    echo 'Show homePage';
+
+//----------
+    //  CONTACTS
+    //----------
+
+} elseif ($explode_url[0] == 'contacts') {
     if (!empty($explode_url[1])) {
         echo "this is the contact n°" . $explode_url[1] . " page";
     } else {
         listContacts();
     }
+
+//----------
+    //  COMPANIES
+    //----------
 
 } elseif ($explode_url[0] == 'companies') {
     if (isset($explode_url[1])) {
@@ -28,6 +44,19 @@ if ($explode_url[0] == 'contacts') {
     } else {
         echo 'show all companies';
     }
+
+//----------
+    //  INVOICES
+    //----------
+
+} elseif ($explode_url[0] == 'invoices') {
+    if (isset($explode_url[1])) {
+        echo "this is the company n°" . $explode_url[1] . " page";
+
+    } else {
+        echo 'show all companies';
+    }
+
 } else {
     echo "Landing main page";
 }
--- views/contactsView.php
+++ views/contactsView.php
@@ -1,3 +1,37 @@
 <?php
 
-echo "listcontacts";
+include '../components/header.php';
+include '../components/navbar.php';
+
+$test = array('manu', 'phil', 'pierre', 'guillaume', 'Jeason');
+
+?>
+
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Firsname</th>
+            <th scope="col">Last nale</th>
+            <th scope="col">telephone</th>
+            <th scope="col">email</th>
+            <th scope="col">working at</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($test as $key): ?>
+        <tr>
+            <th scope="row">1</th>
+            <td><?php echo $key ?></td>
+            <td>Otto</td>
+            <td>telephone N</td>
+            <td>email</td>
+            <td>macdonalds</td>
+        </tr>
+        <?php endforeach?>
+    </tbody>
+</table>
+
+
+<?php
+include '../components/footer.php';
