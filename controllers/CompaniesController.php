1cbc0ee873f0b2da5625d83632bc347d04146374
Pierre Kimbanzi Ruganda <kimbanzip@gmail.com>
2019-10-31 10:40:17 +0100

    add page title to controller

--- controllers/CompaniesController.php
+++ controllers/CompaniesController.php
@@ -5,6 +5,7 @@ function showCompanies()
 {
     require 'models/companies/CompaniesModel.php';
     $companies = getCompanies();
+    $page_title = 'View Companies';
     //print_r($companies);
     include 'views/companies/CompaniesView.php';
 }
@@ -13,6 +14,7 @@ function showCompanyId($id)
 {
     require 'models/companies/CompanyModel.php';
     $company = getCompany($id);
+    $page_title= 'View Company';
     include 'views/companies/CompanyView.php';
 
 }
