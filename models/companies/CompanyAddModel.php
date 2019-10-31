ea38503624abd0e613bd59a37d7c6ee64c5f43c5
Pierre Kimbanzi Ruganda <kimbanzip@gmail.com>
2019-10-31 11:01:43 +0100

    addcompanymodel

---
+++ models/companies/CompanyAddModel.php
@@ -0,0 +1,13 @@
+<?php
+
+function addCompany(){
+    
+    include 'config/db.php';
+    $stmt = $conn->prepare("SELECT * FROM companies");
+    $stmt->execute();
+
+    $row = $stmt->fetchAll();
+    return $row;
+}
+
+addCompany();
