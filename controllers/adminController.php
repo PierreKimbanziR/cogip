0de558a8a59bbd78462dac3935d66e23508b45bd
Philippe Gilles <philgdep@gmail.com>
2019-11-02 14:00:00 +0100

    adminController advance

--- controllers/adminController.php
+++ controllers/adminController.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'models/adminModel.php';
+
 function showDashboard()
 {
     require 'models/contact/contactsModel.php';
@@ -14,25 +16,35 @@ function showDashboard()
 
 function showUsers()
 {
-    //
+    $users = getUsers();
 }
 
 function showUserId($id)
 {
-    //
+    $user = getUser($id);
 }
 
 function createUser()
 {
-    //
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        addUser();
+    } else {
+        createUserView();
+    }
 }
 
 function updateUser($id)
 {
-    //
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        patchUser();
+    } else {
+        getUser($id);
+        createUserView();
+    }
+
 }
 
 function deleteUser($id)
 {
-
+    removeUser($id)();
 }
