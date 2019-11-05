305d261cb3181d470426620af58c8474b305001f
Philippe Gilles <philgdep@gmail.com>
2019-11-05 14:33:56 +0100

    fix select search to all

--- components/scripts.php
+++ components/scripts.php
@@ -9,3 +9,27 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.10/js/mdb.min.js"></script>
 <script type="text/javascript" src="/cogip/static/js/datatables.min.js"></script>
 <script src="/cogip/static/js/select2.min.js"></script>
+<script>
+$('.fa-trash-alt').click(e => {
+    let id = $(e.currentTarget).data('itemid');
+    let name = $(e.currentTarget).data('itemname');
+    let item = $(e.currentTarget).data('item');
+    let link = `/cogip/admin/users/delete/${id}`;
+    $('#deleteModalName').text(name);
+    $('#deleteModalLabel').text("Delete " + item);
+    $('#deleteModalAction').attr("action", link);
+})
+
+// In your Javascript (external .js resource or <script> tag)
+$(document).ready(function() {
+    $('.selectSearch').select2();
+});
+// In your Javascript (external .js resource or <script> tag)
+$(document).ready(function() {
+    $('.selectSearch2').select2();
+});
+// In your Javascript (external .js resource or <script> tag)
+$(document).ready(function() {
+    $('.selectSearch3').select2();
+});
+</script>
