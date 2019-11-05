62193fcdad0c4af301cf224459264492d0ccec42
Pierre Kimbanzi Ruganda <kimbanzip@gmail.com>
2019-11-05 11:53:58 +0100

    add button to view

--- models/companies/patchCompany.php
+++ models/companies/patchCompany.php
@@ -5,3 +5,15 @@ $name_error=$country_error=$vat_error=$role_error="";
 
 
 global $conn; 
+$conn -> exec ('SET NAMES utf8');
+
+$sql="SELECT * FROM companies id= '$id'";
+$stmt= $conn->query($sql);
+$row = $stmt -> fetch(PDO::FETCH_ASSOC);
+
+
+    
+$name=$row["name"];
+$country=$row["country"];
+$vat=$row["vat"];
+$role=$row["role"];
