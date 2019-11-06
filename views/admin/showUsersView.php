c92a375047a208cfedb05d1ed88186b5f7ea71b9
Philippe Gilles <philgdep@gmail.com>
2019-11-06 08:57:29 +0100

    fix delete modale

--- views/admin/showUsersView.php
+++ views/admin/showUsersView.php
@@ -52,29 +52,6 @@ include "components/navbar.php";
         </table>
     </div>
 </div>
-<!-- Modal -->
-<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="deleteModalLabel">Delete</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                Are you sure you want to delete <span class="font-weight-bold" id="deleteModalName"></span>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-                <form id="deleteModalAction" action="" method="post">
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
 
 <?php
 include "components/scripts.php";?>
